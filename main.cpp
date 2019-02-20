8ce2c9f22d83b7c62b2b79f95fc50eb3bfd52f96
Jasmin Mousavi <jasminmousavi@Jasmins-MacBook-Pro.local>
2019-02-20 11:22:40 -0800

    Added documentation to functions

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 /** 
  * Author: Kevin Buffardi & Jasmin Mousavi
  * Course: CSCI 430
+ *
  * A program that takes in 3 numbers and sorts them in descending order
  */
+
 #include <iostream>
+
 using std::cout;
 using std::cin;
 using std::endl;
 
+//Function that takes 3 integers and sorts them in descending order
 void sortDescending(int&,int&,int&);
 
+//Function that takes 2 integers and swaps them
 void swap(int&,int&);
 
+//main code that takes in 3 numbers from user and sorts them in descending order
 int main()
 {
   int numA, numB, numC;
@@ -25,6 +31,15 @@ int main()
   return 0;
 }
 
+/** Function: sortDescending 
+ * Sorts 3 numbers in descending order
+ * Paramenters: 
+ * int& first   -integer used to compare with and sort in descending order
+ * int& second  -integer used to compare with and sort in descending order
+ * int& third   -integer used to compare with and sort in descending order
+ *
+ * returns void
+ */
 void sortDescending(int& first, int& second, int& third)
 {
   if( first < third )
@@ -41,6 +56,14 @@ void sortDescending(int& first, int& second, int& third)
   }
 }
 
+/** Function swap
+ * takes in 2 numbers and swaps them
+ * Parameters:
+ * int& first   -integer used to swap with seocond 
+ * int& second  -integer used to swap with first
+ *
+ * returns void
+ */
 void swap(int &first, int &second)
 {
   int temp = first;
